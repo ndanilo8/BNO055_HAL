ffa90663ab334768838fe1df6c7e5aecdca2e895
ndanilo8 <ndani@PHANTEKS-P600S>
2023-07-21 19:13:51 +0300

    commit test

---
+++ BNO055_HAL/Core/Inc/BNO055_HAL/bno055_hal.cpp
@@ -0,0 +1,8 @@
+/*
+ * bno055_hal.cpp
+ *
+ *  Created on: Jul 4, 2023
+ *      Author: ndanilo8
+ */
+
+#include "bno055_hal.h"
---
+++ BNO055_HAL/Core/Inc/BNO055_HAL/bno055_hal.h
@@ -0,0 +1,15 @@
+/*
+ * bno055_hal.h
+ *
+ *  Created on: Jul 4, 2023
+ *      Author: ndanilo8
+ */
+
+#ifndef INC_SENSORS_BNO055_BNO055_HAL_H_
+#define INC_SENSORS_BNO055_BNO055_HAL_H_
+
+
+
+
+
+#endif /* INC_SENSORS_BNO055_BNO055_HAL_H_ */
